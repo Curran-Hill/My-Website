30342743603c5467fd0626a821ab25be50af2aa8
Curran Hill <currankhill@gmail.com>
2025-02-16 02:46:17 -0600

    New Blog Post on 2025-02-16 02:46:17

--- updateblog.ps1
+++ updateblog.ps1
@@ -5,7 +5,7 @@ $sourcePath = "C:\Users\curra\OneDrive\Documents\Obsidian Vault\My Website"
 $destinationPath = "C:\Users\curra\OneDrive\Documents\My Website\Curran Hill\content\posts"
 
 # Set Github repo URL 
-$myrepo = "https://github.com/Curran-Hill/My-Website.git"
+$myrepo = "git@github.com:Curran-Hill/My-Website.git"
 
 # Set error handling
 $ErrorActionPreference = "Stop"
